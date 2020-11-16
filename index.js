6b09b0ef45472be5546fafd6c3177fa7bc9e4dff
KPGuerra <KPGuerra96@gmail.com>
2020-11-16 01:15:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+  
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor( Math.random() * 1000 ) + 1
+
+const mod = 24 % 5
+
+const max = Math.max( 14, 16, 18, 20 )
+
+
